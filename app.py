61f6466a5c033bda39062fe2a64892b3d0610015
somya <sahajsharma588@gmail.com>
2026-09-15 23:09:31 +0530

    update project

--- app.py
+++ app.py
@@ -1,22 +1,35 @@
-from flask import Flask,render_template, request
-
+from flask import Flask,render_template,request
 import joblib
+import os
 
-app = Flask(__name__,template_folder="templates")
+app = Flask(
+    __name__,
 
-model = joblib.load("student_performance_model.pkl")
+    template_folder=os.path.join(os.path.dirname(os.path.abspath(__file__)),"templates")
+)
+
+model =joblib.load("student_performance_model.pkl")
 
 @app.route("/")
 def home():
-    return render_template("index.html")
-@app.route("/predict",methods=["POST"])
+      return render_template("index.html")
+
+app.route("/predict",methods=["POST"])
 def predict():
-    study_hours = float(request.form["study_hours"])
-    attendance = float(request.form["attendance"])
-    previous_score = float(request.form["previous_score"])
-    assignments = int(request.form["asignment_completed"])
-    input_data = [[study_hours,attendance,previous_score,assignments]]
-    prediction = model.predict(input_data)
-    return f"<h1>predicted final score: {prediction[0]:.2f}</h1>"
+      study_hours=float(request.form["study_hours"])
+      attendance=float(request.form["attendance"])
+      previous_score=float(request.form["previous_score"])
+      assignments=int(request.form["assignments"])
+
+      input_data=[[
+            study_hours,
+            attendance,
+            previous_score,
+            assignments
+      ]]
+
+      prediction = model.predict(input_data)
+      return f"<h1>predicted final score: {prediction[0]:.2f</h1>}"
+
 if __name__=="__main__":
-    app.run(debug=True)
+      app.run(debug=True)
